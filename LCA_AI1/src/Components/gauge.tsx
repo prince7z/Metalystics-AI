4b7243870b46e1943d6371ba57c8f7270d0ffccc
unknown <princesahu17125@gmail.com>
2025-09-11 10:23:12 +0530

    improved

--- LCA_AI1/src/Components/gauge.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Gauge(params: any) {
- const gaugeData = params.data || params;
-    return (<>
-    <h1>Gauge</h1>
-    <pre>{JSON.stringify(gaugeData, null, 2)}</pre>
-    </>)
-}
--- LCA_AI1/src/Components/stratigies.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Strategies(params: any) {
- const strategyData = params.data || params;
-    return (<>
-    <h1>Strategies</h1>
-    <pre>{JSON.stringify(strategyData, null, 2)}</pre>
-    </>)
-}
